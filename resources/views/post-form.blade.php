f07a2c6ce70bea0f5be72d1e452e4d5ba294053f
Ahmad khan <hadijack365@gmail.com>
2026-02-03 08:20:17 +0500

    blog form

--- resources/views/post-form.blade.php
+++ resources/views/post-form.blade.php
@@ -1,3 +1,13 @@
 <div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    <h1>Blogs</h1>
+    <form action="">
+        <div>
+            <label for="title">Title:</label>
+            <input type="text" name="title" placeholder="Title">
+        </div><br><br>
+        <div>
+            <label for="content">Content</label>
+            <textarea name="Content" id=""></textarea>
+        </div>
+    </form>
 </div>
